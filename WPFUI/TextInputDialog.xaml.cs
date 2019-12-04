63f4a1780cf45506c2ebf409cfe8f923bedc309a
Kevin M. Kelly <kevin.kelly@levrum.com>
2019-12-04 13:40:58 -0800

    Added WinForms UI library, changed WPF namespace to be Levrum.UI.WPF to match Levrum.UI.WinForms

--- WPFUI/TextInputDialog.xaml.cs
+++ WPFUI/TextInputDialog.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Levrum.WPFUI
+namespace Levrum.UI.WPF
 {
     /// <summary>
     /// Interaction logic for TextInputDialog.xaml
