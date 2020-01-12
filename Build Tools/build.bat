16958f549dfe7fba150468df3ba12931525a8204
Kevin M. Kelly <progdog@gmail.com>
2020-01-12 13:10:14 -0800

    Added Build Script, Install Script for DataBridge

---
+++ Build Tools/build.bat
@@ -0,0 +1,9 @@
+echo Starting Build...
+call ant -f ant-build-script.xml backup-version-number
+call ant -f ant-build-script.xml -Denv.SVN_REVISION_1=9999 update-version-number
+call ant -f ant-build-script.xml create-output-folder
+msbuild ..\Levrum.sln /t:Rebuild /p:Configuration=Debug
+msbuild ..\Levrum.sln /t:Rebuild /p:Configuration=Release
+call ant -f ant-build-script.xml -Denv.SVN_REVISION_1=9999 build-installer-local
+call ant -f ant-build-script.xml restore-version-number
+echo Build complete. Artifact(s) are located in %CD%\..\output
