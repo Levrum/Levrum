934200b8dfc5a5b7a033204d1ee76d2b3a3e7a0b
Kevin M. Kelly <progdog@gmail.com>
2020-01-12 14:59:50 -0800

    Added support for registering DataMap filetype

--- DataBridge/App.xaml.cs
+++ DataBridge/App.xaml.cs
@@ -6,12 +6,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace DataBridge
+namespace Levrum.DataBridge
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public string StartupFileName { get; set; }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                if (args[1].EndsWith(".dmap"))
+                {
+                    StartupFileName = args[1];
+                }
+            }
+        }
     }
 }
