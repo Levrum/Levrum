1b40a57929f97f1ad65f603a2f509db540787e58
Kevin M. Kelly <progdog@gmail.com>
2020-05-18 15:00:18 -0700

    Fixed bug with MaxLon using Math.Min, threw an extra safeguard in there too.

---
+++ WinFormsUI/HostedControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Levrum.UI.WinForms
+{
+    public abstract class HostedControl : UserControl
+    {
+        public XamlHostForm HostForm { get; set; }
+
+        public HostedControl(XamlHostForm _hostForm = null)
+        {
+            HostForm = _hostForm;
+        }
+
+        public Type ResultType { get { return Result.GetType(); } }
+
+        public virtual object Result { get; set; } = null;
+        public virtual void HostClosing(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormsUI/XamlHostForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Xaml;
+
+
+
+namespace Levrum.UI.WinForms
+{
+    public partial class XamlHostForm : Form
+    {
+        public ElementHost Host { get; protected set; } = new ElementHost();
+
+        HostedControl m_childControl = null;
+        public HostedControl ChildControl
+        {
+            get { return m_childControl; }
+            set
+            {
+                if (value != null)
+                {
+                    m_childControl = value;
+                    double height = value.Height;
+                    double width = value.Width;
+
+                    Host.Child = value;
+                    value.HostForm = this;
+
+                    Height = (int)height;
+                    Width = (int)width;
+
+                    FormClosing += value.HostClosing;
+                } else if (m_childControl != null)
+                {
+                    FormClosing -= m_childControl.HostClosing;
+                    Host.Child = null;
+                    m_childControl.HostForm = null;
+                    m_childControl = null;
+                }
+            }
+        }
+
+        public XamlHostForm(HostedControl _childControl = null)
+        {
+            InitializeComponent();
+            Host.Dock = DockStyle.Fill;
+
+            ChildControl = _childControl;
+
+            Controls.Add(Host);
+        }
+    }
+}
